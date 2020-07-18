9313528dc72a2159075fe463a42acbad02eb3b00
Shreyas <shreyasatul.pophli@gmail.com>
2020-07-19 01:01:43 +0530

    Adding delete node(head/tail/intermediate from dll

--- DLL/DLL/src/DLL.cpp
+++ DLL/DLL/src/DLL.cpp
@@ -63,11 +63,76 @@ void Print()
 	cout<<endl;
 }
 
+int Length()
+{
+	Node* curr = head;
+	int cnt = 0;
+	if(!curr)
+	{
+		return cnt;
+	}
+	else
+	{
+		while(curr)
+		{
+			cnt++;
+			curr=curr->next;
+		}
+		return cnt;
+	}
+}
+
+Node* Delete(int pos)    //Position '1' is head
+{
+	if(pos==0 && pos>Length())
+	{
+		cout<<"Invalid position"<<endl;
+		return NULL;
+	}
+	else if(pos==1)  //Delete head
+	{
+		if(head)
+		{
+			Node*tmp = head;
+			head = tmp->next;
+			delete tmp;
+			return head;
+		}
+		else
+		{
+			cout<<"No element in list"<<endl;
+		}
+	}
+	else{
+		if(head)
+		{
+			Node*tmp = head;
+			while(--pos && tmp)
+			{
+				tmp=tmp->next;
+			}
+			Node* tmp2 = tmp->prev;
+			tmp2->next = tmp->next;
+			if(tmp->next)
+			{
+				tmp->next->prev = tmp2;
+			}
+			delete tmp;
+			return head;
+		}
+	}
+}
+
 int main() {
 	Node* ptr = Init(10);
 	Append(20);
 	Append(30);
 	Append(40);
 	Print();
+	Delete(2);
+	Print();
+	Delete(1);
+	Print();
+	cout<<Length()<<endl;
 	return 0;
 }
